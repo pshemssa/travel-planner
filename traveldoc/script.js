204bfe24858d08ada235d6c9fd8db31b0bec9e98
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-08-21 17:11:15 +0000

    API

--- traveldoc/script.js
+++ traveldoc/script.js
@@ -1,21 +1,17 @@
-const apiKey = '6259f0c19a4f72c3ee257d76d3d8c63c'; // Replace with your actual API key
-
-const cityInput = document.getElementById('city-input');
-const getTemperatureBtn = document.getElementById('get-temperature-btn');
-const temperatureDisplay = document.getElementById('temperature-display');
+// ...
 
 getTemperatureBtn.addEventListener('click', async () => {
   const city = cityInput.value;
   
   if (city) {
-    const temperature = await fetchTemperature(city);
-    temperatureDisplay.textContent = `Current temperature in ${city}: ${temperature}°C`;
+    const weatherData = await fetchWeather(city);
+    displayWeatherData(weatherData);
   } else {
     temperatureDisplay.textContent = 'Please enter a city.';
   }
 });
 
-async function fetchTemperature(city) {
+async function fetchWeather(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   
   try {
@@ -23,12 +19,23 @@ async function fetchTemperature(city) {
     const data = await response.json();
     
     if (response.ok) {
-      return data.main.temp;
+      return {
+        temperature: data.main.temp,
+        description: data.weather[0].description
+      };
     } else {
-      throw new Error(data.message || 'Failed to fetch temperature.');
+      throw new Error(data.message || 'Failed to fetch weather data.');
     }
   } catch (error) {
-    console.error('Error fetching temperature:', error);
+    console.error('Error fetching weather data:', error);
     throw error;
   }
 }
+
+function displayWeatherData(weatherData) {
+  temperatureDisplay.textContent = `Current temperature in ${city}: ${weatherData.temperature}°C`;
+  // Display more weather information as needed
+  // Example: temperatureDisplay.textContent += `, Description: ${weatherData.description}`;
+}
+
+// ...
